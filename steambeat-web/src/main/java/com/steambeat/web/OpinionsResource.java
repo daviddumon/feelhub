0d7748f367860dc003302a41eb8d1b84ae795855
david <dumon.david@gmail.com>
2011-12-02 18:26:17 +0100

    red

--- steambeat-web/src/main/java/com/steambeat/web/OpinionsResource.java
+++ steambeat-web/src/main/java/com/steambeat/web/OpinionsResource.java
@@ -12,12 +12,12 @@ public class OpinionsResource extends ServerResource {
     @Post
     public void post(final Form form) {
         final String text = form.getFirstValue("text");
-        List<Judgment> judgments = Lists.newArrayList();
-        final ListIterator<Parameter> parameterListIterator = form.listIterator();
-        while (parameterListIterator.hasNext()) {
-            final Parameter next = parameterListIterator.next();
-            judgments.add(new Judgment(next.getFirst(), Feeling.valueOf(next.getSecond())));
-        }
+        //List<Judgment> judgments = Lists.newArrayList();
+        //final ListIterator<Parameter> parameterListIterator = form.listIterator();
+        //while (parameterListIterator.hasNext()) {
+        //    final Parameter next = parameterListIterator.next();
+        //    judgments.add(new Judgment(next.getFirst(), Feeling.valueOf(next.getSecond())));
+        //}
         setStatus(Status.SUCCESS_CREATED);
     }
 }
