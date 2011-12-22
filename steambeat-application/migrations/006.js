9cfe7ca8d47c47222206266f5c3f9e77e60d571f
david <dumon.david@gmail.com>
2011-12-22 11:53:26 +0100

    migration 006

--- steambeat-application/migrations/006.js
+++ steambeat-application/migrations/006.js
@@ -1,17 +1,16 @@
-var i = 0;
 db.opinion.find().forEach(function (opinion) {
-    if (opinion.subjectId = "http://www.kikiyoo.com") {
-        print(opinion.subjectId);
+    if (opinion.subjectId == "http://www.kikiyoo.com") {
         db.opinion.update({"_id":opinion._id}, {$set:{"subjectId":"http://www.steambeat.com"}}, false, true);
     }
 });
-print(i);
 
 db.opinion.find().forEach(function (opinion) {
-    db.opinion.update({"_id":opinion._id}, {$push:{"judgments":{"feeling":opinion.feeling, "subjectId":opinion.subjectId}}}, false, true);
-    //db.opinion.update({"_id":opinion._id}, {$unset:{"feeling":1}}, false, true);
-    //db.opinion.update({"_id":opinion._id}, {$unset:{"subjectId":1}}, false, true);
+    db.opinion.update({"_id":opinion._id}, {$set:{"judgments":[
+        {"feeling":opinion.feeling, "subjectId":opinion.subjectId}
+    ]}});
 });
 
-
-
+db.opinion.find().forEach(function (opinion) {
+    db.opinion.update({"_id":opinion._id}, {$unset:{"feeling":1}});
+    db.opinion.update({"_id":opinion._id}, {$unset:{"subjectId":1}});
+});
