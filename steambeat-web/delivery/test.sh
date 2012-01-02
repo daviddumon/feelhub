e302dfe295fa086689f152cba87d98a570667f29
david <dumon.david@gmail.com>
2012-01-02 23:38:52 +0100

    livraison v2

--- steambeat-web/delivery/test.sh
+++ steambeat-web/delivery/test.sh
@@ -1,2 +1,6 @@
 #/bin/sh
 scp -P 22 target/steambeat-web-0.0.1-SNAPSHOT.war root@10.0.0.7:/opt/tomcat/test.bytedojo.com/steambeat.war
+ssh root@10.0.0.7 'chmod +x /opt/tomcat/test.bytedojo.com/steambeat.war'
+
+# set permission sur le war
+# drop database dans mongo
