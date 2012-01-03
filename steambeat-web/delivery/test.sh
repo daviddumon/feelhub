563dbfaef805e89a0e965d98cd5fb9ce9f89db55
david <dumon.david@gmail.com>
2012-01-03 10:48:48 +0100

    livraison v4

--- steambeat-web/delivery/test.sh
+++ steambeat-web/delivery/test.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 scp -P 22 target/steambeat-web-0.0.1-SNAPSHOT.war root@10.0.0.7:/opt/tomcat/test.bytedojo.com/steambeat.war
+scp -P 22 delivery/drop.js root@10.0.0.7:
 ssh root@10.0.0.7 'chmod +x /opt/tomcat/test.bytedojo.com/steambeat.war'
-ssh root@10.0.0.7 'mongo steambeat delivery/drop.js'
+ssh root@10.0.0.7 'mongo /root/drop.js steambeat'
