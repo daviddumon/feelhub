d8aa6fbb770a1fc1b6e7ae23eed898a128c8132e
david <dumon.david@gmail.com>
2012-03-06 16:53:16 +0100

    bad semantic in url -> 301

--- steambeat-web/src/main/java/com/steambeat/web/resources/WebPageResource.java
+++ steambeat-web/src/main/java/com/steambeat/web/resources/WebPageResource.java
@@ -3,8 +3,9 @@ package com.steambeat.web.resources;
 import com.google.inject.Inject;
 import com.steambeat.application.WebPageService;
 import com.steambeat.domain.subject.webpage.WebPage;
-import com.steambeat.web.SteambeatTemplateRepresentation;
-import org.restlet.representation.Representation;
+import com.steambeat.web.*;
+import org.restlet.data.Status;
+import org.restlet.representation.*;
 import org.restlet.resource.*;
 
 import java.util.UUID;
@@ -20,16 +21,27 @@ public class WebPageResource extends ServerResource {
     protected void doInit() throws ResourceException {
         id = UUID.fromString(getRequestAttributes().get("id").toString());
         webPage = webPageService.lookUpWebPage(id);
+        semantic = getRequestAttributes().get("semantic").toString();
+        if (!semantic.equals(webPage.getSemanticDescription())) {
+            mustRedirect = true;
+        }
     }
 
     @Get
     public Representation represent() {
-        return SteambeatTemplateRepresentation.createNew("webpage.ftl", getContext())
-                .with("webPage", webPage)
-                .with("counter", 0);
+        if (mustRedirect) {
+            setStatus(Status.REDIRECTION_PERMANENT);
+            final String uriToRedirect = new ReferenceBuilder(getContext()).buildUri("/webpages/" + webPage.getSemanticDescription() + "/" + webPage.getId());
+            setLocationRef(uriToRedirect);
+            return new EmptyRepresentation();
+        } else {
+            return SteambeatTemplateRepresentation.createNew("webpage.ftl", getContext()).with("webPage", webPage).with("counter", 0);
+        }
     }
 
     private final WebPageService webPageService;
     private UUID id;
     private WebPage webPage;
+    private String semantic;
+    private boolean mustRedirect = false;
 }
--- steambeat-web/src/test/java/com/steambeat/web/resources/TestsWebPageResource.java
+++ steambeat-web/src/test/java/com/steambeat/web/resources/TestsWebPageResource.java
@@ -5,6 +5,7 @@ import com.steambeat.test.fakeRepositories.WithFakeRepositories;
 import com.steambeat.test.testFactories.TestFactories;
 import com.steambeat.web.*;
 import org.junit.*;
+import org.restlet.Context;
 import org.restlet.data.Status;
 import org.restlet.representation.Representation;
 
@@ -25,7 +26,7 @@ public class TestsWebPageResource {
     @Test
     public void isMapped() throws IOException {
         final WebPage webPage = TestFactories.webPages().newWebPage();
-        final ClientResource webpageResource = restlet.newClientResource("/webpages/semantic/" + webPage.getId());
+        final ClientResource webpageResource = restlet.newClientResource("/webpages/" + webPage.getSemanticDescription() + "/" + webPage.getId());
 
         final Representation response = webpageResource.get();
 
@@ -44,11 +45,23 @@ public class TestsWebPageResource {
     @Test
     public void canRepresentExistingWebPage() {
         final WebPage webPage = TestFactories.webPages().newWebPage();
-        final ClientResource webpageResource = restlet.newClientResource("/webpages/semantic/" + webPage.getId());
+        final ClientResource webpageResource = restlet.newClientResource("/webpages/" + webPage.getSemanticDescription() + "/" + webPage.getId());
 
         final SteambeatTemplateRepresentation representation = (SteambeatTemplateRepresentation) webpageResource.get();
 
         final Map<String, Object> dataModel = representation.getDataModel();
         assertThat(dataModel, hasEntry("webPage", (Object) webPage));
+    }
+
+    @Test
+    public void permanentRedirectOnBadSemantic() {
+        final WebPage webPage = TestFactories.webPages().newWebPage();
+        final ClientResource webpageResource = restlet.newClientResource("/webpages/semantic/" + webPage.getId());
+
+        webpageResource.get();
+
+        assertThat(webpageResource.getStatus(), is(Status.REDIRECTION_PERMANENT));
+        final String uriToRedirect = new ReferenceBuilder(Context.getCurrent()).buildUri("/webpages/" + webPage.getSemanticDescription() + "/" + webPage.getId());
+        assertThat(webpageResource.getLocationRef().toString(), is(uriToRedirect));
     }
 }
