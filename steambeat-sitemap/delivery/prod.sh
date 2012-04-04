93c3d153a04288d000e4e67a96a55e5cd9d37702
david <dumon.david@gmail.com>
2012-04-04 14:53:25 +0100

    deploy script sitemaps

--- steambeat-sitemap/delivery/prod.sh
+++ steambeat-sitemap/delivery/prod.sh
@@ -1,3 +1,4 @@
 #/bin/sh
+sudo chmod 600 ../aws/certifications/steambeat.pem
 scp -P 22 -i ../aws/certifications/steambeat.pem target/steambeat-sitemap-0.0.1-SNAPSHOT.war ubuntu@23.21.113.241:/var/lib/tomcat7/webapps/root.war
 ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'chmod +x /var/lib/tomcat7/webapps/root.war'
