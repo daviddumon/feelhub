4e1ac2dee08fdfb3a0b829d4cce7154ebbbbde1f
david <dumon.david@gmail.com>
2012-04-04 15:33:34 +0100

    naming of sitemap war

--- steambeat-sitemap/delivery/prod.sh
+++ steambeat-sitemap/delivery/prod.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 chmod 600 ../aws/certifications/steambeat.pem
-scp -P 22 -i ../aws/certifications/steambeat.pem target/steambeat-sitemap-0.0.1-SNAPSHOT.war ubuntu@23.21.113.241:/var/lib/tomcat7/webapps/root.war
-ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'chmod +x /var/lib/tomcat7/webapps/root.war'
+scp -P 22 -i ../aws/certifications/steambeat.pem target/steambeat-sitemap-0.0.1-SNAPSHOT.war ubuntu@23.21.113.241:/var/lib/tomcat7/webapps/ROOT.war
+ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'chmod +x /var/lib/tomcat7/webapps/ROOT.war'
