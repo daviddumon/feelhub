9ff2cfe6962169da31f3e5e28846d19d0d3cc913
david <dumon.david@gmail.com>
2012-04-05 11:54:01 +0100

    wrong argument number

--- steambeat-web/delivery/deploy_production.sh
+++ steambeat-web/delivery/deploy_production.sh
@@ -4,7 +4,7 @@ bucket=elasticbeanstalk-us-east-1-952638247896
 file=steambeat-$1.war
 
 # upload war to S3 bucket
-s3cmd put $file  s3://$bucket/$file
+s3cmd put target/$file  s3://$bucket/$file
 
 # create a new application for elastic beanstalk
 elastic-beanstalk-create-application-version -a Steambeat -d $file -l $file -s elasticbeanstalk-us-east-1-952638247896/$file
