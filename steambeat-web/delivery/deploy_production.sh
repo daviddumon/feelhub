d467e14fff6d7bfae84488623f4f2b46c3865b72
david <dumon.david@gmail.com>
2012-04-05 16:28:55 +0100

    deploy script deploys ...

--- steambeat-web/delivery/deploy_production.sh
+++ steambeat-web/delivery/deploy_production.sh
@@ -9,3 +9,6 @@ s3cmd put target/$file  s3://$bucket/$file
 
 # create a new application for elastic beanstalk
 /opt/elasticbeanstalk-cli/bin/elastic-beanstalk-create-application-version -a Steambeat -d $file -l $file -s elasticbeanstalk-us-east-1-952638247896/$file
+
+# use the new version on elastic beanstalk
+/opt/elasticbeanstalk-cli/bin/elastic-beanstalk-update-environment -e steambeat -l $file
