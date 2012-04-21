cb167f0c7f7d87dd7745bdce117fc464e807d0fa
david <dumon.david@gmail.com>
2012-04-21 18:25:59 +0100

    red ci

--- steambeat-application/src/test/java/com/steambeat/test/testFactories/SubjectFactoryForTest.java
+++ steambeat-application/src/test/java/com/steambeat/test/testFactories/SubjectFactoryForTest.java
@@ -1,14 +1,15 @@
 package com.steambeat.test.testFactories;
 
 import com.steambeat.domain.analytics.Association;
+import com.steambeat.domain.analytics.alchemy.readmodel.AlchemyJsonEntity;
 import com.steambeat.domain.analytics.identifiers.uri.Uri;
-import com.steambeat.domain.subject.concept.Concept;
+import com.steambeat.domain.subject.concept.*;
 import com.steambeat.domain.subject.steam.Steam;
 import com.steambeat.domain.subject.webpage.WebPage;
 import com.steambeat.repositories.Repositories;
 import com.steambeat.test.FakeUriScraper;
 
-import java.util.UUID;
+import java.util.*;
 
 public class SubjectFactoryForTest {
 
@@ -33,7 +34,8 @@ public class SubjectFactoryForTest {
     }
 
     public Concept newConcept() {
-        final Concept concept = new Concept("my concept");
+        final List<AlchemyJsonEntity> entities = TestFactories.alchemy().entities(1);
+        final Concept concept = new ConceptFactory().newConcept(entities.get(0));
         concept.setScraper(new FakeUriScraper());
         Repositories.subjects().add(concept);
         return concept;
