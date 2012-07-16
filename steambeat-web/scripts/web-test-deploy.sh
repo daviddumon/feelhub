d1e257065b03f1fd4d9e73b7c602f5b774b00519
david <dumon.david@gmail.com>
2012-07-16 12:34:40 +0200

    script de deploiement web test

--- steambeat-web/scripts/web-test-deploy.sh
+++ steambeat-web/scripts/web-test-deploy.sh
@@ -2,6 +2,6 @@
 echo 'test-deploy script argument : ' $1
 file=steambeat-$1.war
 scp -P 22 target/$file root@10.0.0.7:/opt/tomcat/test.bytedojo.com/steambeat.war
-scp -P 22 delivery/drop.js root@10.0.0.7:
+scp -P 22 scripts/drop.js root@10.0.0.7:
 ssh root@10.0.0.7 'chmod +x /opt/tomcat/test.bytedojo.com/steambeat.war'
 ssh root@10.0.0.7 'mongo steambeat /root/drop.js'
