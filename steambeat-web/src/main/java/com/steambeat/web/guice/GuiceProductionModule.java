0ca6d36e9e0b0884fe97084a05a33117d495f05d
david <dumon.david@gmail.com>
2012-08-22 10:38:16 +0200

    Instantiation de tous les listeners

--- steambeat-web/src/main/java/com/steambeat/web/guice/GuiceProductionModule.java
+++ steambeat-web/src/main/java/com/steambeat/web/guice/GuiceProductionModule.java
@@ -8,10 +8,12 @@ import com.steambeat.domain.bingsearch.BingLink;
 import com.steambeat.domain.concept.ConceptTranslator;
 import com.steambeat.domain.eventbus.DeadEventCatcher;
 import com.steambeat.domain.illustration.IllustrationManager;
-import com.steambeat.domain.keyword.KeywordFactory;
+import com.steambeat.domain.keyword.*;
+import com.steambeat.domain.opinion.OpinionManager;
 import com.steambeat.domain.reference.*;
+import com.steambeat.domain.relation.RelationManager;
 import com.steambeat.domain.scrapers.UriScraper;
-import com.steambeat.domain.statistics.StatisticsFactory;
+import com.steambeat.domain.statistics.*;
 import com.steambeat.repositories.SessionProvider;
 import com.steambeat.web.mail.*;
 
@@ -19,18 +21,21 @@ public class GuiceProductionModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(StatisticsFactory.class).toInstance(new StatisticsFactory());
         bind(AlchemyEntityAnalyzer.class).toInstance(new AlchemyEntityAnalyzer(new NamedEntityJsonProvider(new AlchemyLink(), new NamedEntityBuilder(new KeywordService(new KeywordFactory(), new ReferenceFactory()))), new KeywordService(new KeywordFactory(), new ReferenceFactory())));
         bind(UriScraper.class).toInstance(new UriScraper());
         bind(NamedEntityProvider.class).toInstance(new NamedEntityJsonProvider(new AlchemyLink(), new NamedEntityBuilder(new KeywordService(new KeywordFactory(), new ReferenceFactory()))));
         bind(MailBuilder.class).toInstance(new MailBuilder(new MailSender()));
         bind(DeadEventCatcher.class).toInstance(new DeadEventCatcher());
 
-
         bind(SubjectIdentifier.class).toInstance(new SubjectIdentifier());
         bind(ConceptTranslator.class).toInstance(new ConceptTranslator(new KeywordService(new KeywordFactory(), new ReferenceFactory())));
         bind(ReferenceManager.class).toInstance(new ReferenceManager());
         bind(IllustrationManager.class).toInstance(new IllustrationManager(new BingLink()));
+        bind(StatisticsFactory.class).toInstance(new StatisticsFactory());
+        bind(KeywordManager.class).toInstance(new KeywordManager());
+        bind(OpinionManager.class).toInstance(new OpinionManager());
+        bind(RelationManager.class).toInstance(new RelationManager());
+        bind(StatisticsManager.class).toInstance(new StatisticsManager());
     }
 
     @Provides
