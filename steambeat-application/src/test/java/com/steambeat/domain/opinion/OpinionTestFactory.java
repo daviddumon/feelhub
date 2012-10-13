326171044c627b1a5269602615ba0b090051b4c7
david <dumon.david@gmail.com>
2012-10-13 12:00:33 +0200

    fix ci

--- steambeat-application/src/test/java/com/steambeat/domain/opinion/OpinionTestFactory.java
+++ steambeat-application/src/test/java/com/steambeat/domain/opinion/OpinionTestFactory.java
@@ -8,15 +8,26 @@ import com.steambeat.test.TestFactories;
 public class OpinionTestFactory {
 
     public Opinion newOpinion() {
-        return newOpinionWithText("my good opinion");
+        return newOpinionWithText("text");
     }
 
     public Opinion newOpinionWithText(final String text) {
         return newOpinion(TestFactories.references().newReference(), text);
     }
 
+    public void newOpinions(final int quantity) {
+        final Reference reference = TestFactories.references().newReference();
+        newOpinions(reference, quantity);
+    }
+
+    public void newOpinions(final Reference reference, final int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            newOpinion(reference, "i" + i);
+        }
+    }
+
     private Opinion newOpinion(final Reference reference, final String text) {
-        final User activeUser = TestFactories.users().createActiveUser("userforopinion@mail.com");
+        final User activeUser = TestFactories.users().createActiveUser(text + "userforopinion@mail.com");
         final Opinion opinion = new Opinion(text, activeUser);
         opinion.addJudgment(reference, Feeling.bad);
         Repositories.opinions().add(opinion);
@@ -36,16 +47,5 @@ public class OpinionTestFactory {
         final Opinion opinion = new Opinion("opinion without judgement", activeUser);
         Repositories.opinions().add(opinion);
         return opinion;
-    }
-
-    public void newOpinions(final int quantity) {
-        final Reference reference = TestFactories.references().newReference();
-        newOpinions(reference, quantity);
-    }
-
-    public void newOpinions(final Reference reference, final int quantity) {
-        for (int i = 0; i < quantity; i++) {
-            newOpinion(reference, "i" + i);
-        }
     }
 }
