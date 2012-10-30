5bd78eac0c3f777d7ec10d54f4937fde0143227c
david <dumon.david@gmail.com>
2012-10-30 19:28:49 +0100

    sitemap deploy script

--- feelhub-sitemap/scripts/sitemap-production-deploy.sh
+++ feelhub-sitemap/scripts/sitemap-production-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-chmod 600 ../aws/certifications/feelhub.pem
-ssh -i ../aws/certifications/feelhub.pem ubuntu@23.21.113.241 'sudo /etc/init.d/tomcat7 stop'
-ssh -i ../aws/certifications/feelhub.pem ubuntu@23.21.113.241 'sudo rm -rf /var/lib/tomcat7/webapps/ROOT*'
-scp -P 22 -i ../aws/certifications/feelhub.pem target/feelhub-sitemap-0.0.1-SNAPSHOT.war ubuntu@23.21.113.241:/var/lib/tomcat7/webapps/ROOT.war
-ssh -i ../aws/certifications/feelhub.pem ubuntu@23.21.113.241 'chmod +x /var/lib/tomcat7/webapps/ROOT.war'
-ssh -i ../aws/certifications/feelhub.pem ubuntu@23.21.113.241 'sudo /etc/init.d/tomcat7 start'
+chmod 600 ../aws/certifications/steambeat.pem
+ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'sudo /etc/init.d/tomcat7 stop'
+ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'sudo rm -rf /var/lib/tomcat7/webapps/ROOT*'
+scp -P 22 -i ../aws/certifications/steambeat.pem target/feelhub-sitemap-0.0.1-SNAPSHOT.war ubuntu@23.21.113.241:/var/lib/tomcat7/webapps/ROOT.war
+ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'chmod +x /var/lib/tomcat7/webapps/ROOT.war'
+ssh -i ../aws/certifications/steambeat.pem ubuntu@23.21.113.241 'sudo /etc/init.d/tomcat7 start'
