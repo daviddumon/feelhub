c4bfc1756ccfcad2d8793b53620154ec5bf2d43f
bodysplash <bodysplash@gmail.com>
2012-11-06 21:25:02 +0100

    manque un commit

--- feelhub-web/src/main/java/com/feelhub/web/resources/social/FacebookResource.java
+++ feelhub-web/src/main/java/com/feelhub/web/resources/social/FacebookResource.java
@@ -1,13 +1,13 @@
 package com.feelhub.web.resources.social;
 
 import com.feelhub.application.UserService;
-import com.feelhub.web.ReferenceBuilder;
-import com.feelhub.web.authentification.*;
+import com.feelhub.web.authentification.AuthRequest;
+import com.feelhub.web.authentification.AuthenticationManager;
 import com.feelhub.web.social.FacebookConnector;
 import com.google.inject.Inject;
 import com.restfb.types.User;
-import org.restlet.data.Status;
-import org.restlet.resource.*;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
 import org.scribe.model.Token;
 
 public class FacebookResource extends ServerResource {
@@ -26,8 +26,8 @@ public class FacebookResource extends ServerResource {
         final User facebookUser = connector.getUser(accesToken);
         final com.feelhub.domain.user.User newUser = userService.createFromFacebook(facebookUser.getId(), facebookUser.getEmail(), facebookUser.getFirstName(), facebookUser.getLastName(), facebookUser.getLocale());
         authenticationManager.authenticate(AuthRequest.facebook(newUser.getId()));
-        setLocationRef(new ReferenceBuilder(getContext()).buildUri(""));
-        setStatus(Status.REDIRECTION_TEMPORARY);
+        //setLocationRef(new ReferenceBuilder(getContext()).buildUri(""));
+       // setStatus(Status.REDIRECTION_TEMPORARY);
     }
 
     private FacebookConnector connector;
