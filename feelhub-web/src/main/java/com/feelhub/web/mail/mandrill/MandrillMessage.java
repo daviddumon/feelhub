e96d946ee54f8977d4ec96e3d8d3c33babe44197
bodysplash <bodysplash@gmail.com>
2012-11-25 17:38:57 +0100

    Envoi de mail via mailchimp / mandrill
    merge

---
+++ feelhub-web/src/main/java/com/feelhub/web/mail/mandrill/MandrillMessage.java
@@ -0,0 +1,22 @@
+package com.feelhub.web.mail.mandrill;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class MandrillMessage {
+
+    public List<MandrillRecipient> to = Lists.newArrayList();
+    public String subject;
+    public String from_email;
+    public List<MergeVar> global_merge_vars = Lists.newArrayList();
+    public List<String> tags = Lists.newArrayList();
+
+    public void addMergeVar(String name, String content) {
+        global_merge_vars.add(new MergeVar(name, content));
+    }
+
+    public void addTag(final String tag) {
+        tags.add(tag);
+    }
+}
