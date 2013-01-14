4e0c963e230da52a34825047ac53618f41e10d3f
david <dumon.david@gmail.com>
2013-01-14 19:38:45 +0100

    feelings in home

--- feelhub-web/src/main/webapp/static/js/controller/home-controller.js
+++ feelhub-web/src/main/webapp/static/js/controller/home-controller.js
@@ -2,12 +2,20 @@ require.config(
     {
         paths: {
             'jquery': 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
+            'view': '../view',
             'modules': "../modules",
-            "plugins": "../plugins"
+            "plugins": "../plugins",
+            "templates": "../view/templates",
+            "hogan": "../plugins/hogan",
+            "text": "../plugins/text"
         }
     }
 );
 
-require(["plugins/domReady!", "modules/interface"], function (doc, interface) {
-    interface.init();
-});
+require(["plugins/domReady!", "modules/interface", "modules/flow", "view/flow/feeling-view"],
+
+    function (doc, interface, flow, view) {
+        var list_feeling_api_end_point = root + "/api/feelings";
+        interface.init();
+        flow.init(list_feeling_api_end_point, null, view, null);
+    });
