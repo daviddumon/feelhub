a797b63f89c8dc85e448a90d9bd9e56f71c120bb
Charles Couillard <charles.couillard@arpinum.fr>
2013-01-25 17:08:06 +0100

    correction sur le calcul du score (tests)

--- feelhub-application/src/test/java/com/feelhub/domain/topic/TestsTopic.java
+++ feelhub-application/src/test/java/com/feelhub/domain/topic/TestsTopic.java
@@ -2,23 +2,26 @@ package com.feelhub.domain.topic;
 
 import com.feelhub.domain.eventbus.WithDomainEvent;
 import com.feelhub.domain.feeling.Feeling;
+import com.feelhub.domain.feeling.Sentiment;
+import com.feelhub.domain.feeling.SentimentValue;
 import com.feelhub.domain.tag.Tag;
 import com.feelhub.domain.thesaurus.FeelhubLanguage;
-import com.feelhub.domain.topic.ftp.FtpTopic;
-import com.feelhub.domain.topic.http.*;
+import com.feelhub.domain.topic.http.HttpTopic;
+import com.feelhub.domain.topic.http.HttpTopicType;
 import com.feelhub.domain.topic.http.uri.Uri;
+import com.feelhub.domain.topic.real.RealTopic;
 import com.feelhub.domain.user.User;
 import com.feelhub.repositories.fakeRepositories.WithFakeRepositories;
 import com.feelhub.test.SystemTime;
 import com.feelhub.test.TestFactories;
 import com.google.inject.Injector;
 import org.joda.time.DateTime;
-import org.junit.*;
+import org.junit.Rule;
+import org.junit.Test;
 
-import java.util.List;
 import java.util.UUID;
 
-import static org.fest.assertions.Assertions.*;
+import static org.fest.assertions.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class TestsTopic {
