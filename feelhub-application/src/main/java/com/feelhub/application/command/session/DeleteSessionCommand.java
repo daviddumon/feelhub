0d12fe8a13c2962cf5c681bba4b6edce07de9e23
jb <bodysplash@gmail.com>
2013-03-07 07:09:51 +0100

    oubli du fichier

---
+++ feelhub-application/src/main/java/com/feelhub/application/command/session/DeleteSessionCommand.java
@@ -0,0 +1,29 @@
+package com.feelhub.application.command.session;
+
+import com.feelhub.application.command.Command;
+import com.feelhub.domain.session.Session;
+import com.feelhub.repositories.Repositories;
+
+import java.util.UUID;
+
+public class DeleteSessionCommand implements Command<Object> {
+
+    public DeleteSessionCommand(UUID token) {
+        this.token = token;
+    }
+
+    @Override
+    public Object execute() {
+        final Session session = lookUpSession(token);
+        if (session != null) {
+            Repositories.sessions().delete(session);
+        }
+        return null;
+    }
+
+    private Session lookUpSession(final UUID token) {
+        return Repositories.sessions().get(token);
+    }
+
+    public final UUID token;
+}
