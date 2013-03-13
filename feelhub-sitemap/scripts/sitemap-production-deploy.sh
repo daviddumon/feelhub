bda2a415fc24a5b0a07878a7640d2e91683aaed8
charles <couillardcharles@gmail.com>
2013-03-13 22:17:28 +0100

    script bcp plus simple

--- feelhub-sitemap/scripts/sitemap-production-deploy.sh
+++ feelhub-sitemap/scripts/sitemap-production-deploy.sh
@@ -2,7 +2,5 @@
 echo 'production-deploy script argument : ' $1
 
 file=feelhub-sitemap-$1-jar-with-dependencies.jar
-host=ubuntu@ec2-54-235-59-50.compute-1.amazonaws.com
-identity_file=???/certifications/feelhub.pem
 
-scp -i $identity_file target/$file $host:/home/ubuntu/sitemaps/feelhub-sitemap-jar-with-dependencies.jar
+cp target/$file /home/ubuntu/sitemaps/feelhub-sitemap-jar-with-dependencies.jar
