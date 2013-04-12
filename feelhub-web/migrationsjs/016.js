e6cfd82cf7f22ee056fb7f78f989948cf7a5341b
david <dumon.david@gmail.com>
2013-04-12 19:42:29 +0200

    016.js final

--- feelhub-web/migrationsjs/016.js
+++ feelhub-web/migrationsjs/016.js
@@ -1,40 +1,50 @@
+var begin = new Date();
+var FoundException = {};
 rs.slaveOk();
 
 var topics = 0, deleted = 0, keeped = 0;
 
 function in_feeling(id) {
-    var result = false;
-    db.feeling.find().forEach(function (feeling) {
-        feeling.sentiments.forEach(function (sentiment) {
-            if (sentiment.topicId.hex() === id.hex()) {
-                result = true;
-            }
+    try {
+        db.feeling.find().forEach(function (feeling) {
+            feeling.sentiments.forEach(function (sentiment) {
+                if (sentiment.topicId.hex() === id.hex()) {
+                    throw new FoundException();
+                }
+            });
         });
-    });
-    return result;
+        return false;
+    } catch (e) {
+        return true;
+    }
 }
 
 function in_related(id) {
-    var result = false;
-    db.related.find().forEach(function (related) {
-        if(related.toId.hex() === id.hex()) {
-            result = true;
-        }
-    });
-    return result;
+    try {
+        db.related.find().forEach(function (related) {
+            if (related.toId.hex() === id.hex()) {
+                throw new FoundException();
+            }
+        });
+        return false;
+    } catch (e) {
+        return true;
+    }
 }
 
 db.topic.find().forEach(function (topic) {
+    print("topic : " + topic._id);
     if (in_feeling(topic._id) || in_related(topic._id)) {
         keeped++;
     } else {
         db.topic.remove({_id: topic._id});
         deleted++;
+        print("deleted");
     }
     topics++;
-    print("topic : " + topic._id);
 });
 
 print("total : " + topics);
 print("delete : " + deleted);
 print("keeped : " + keeped);
+print("time : " + (new Date() - begin));
