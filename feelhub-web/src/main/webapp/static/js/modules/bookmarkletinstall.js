8483e797bf812be23c6f5f6516441b4a30a39323
david <dumon.david@gmail.com>
2013-10-19 10:05:26 +0200

    opera bookmarklet install

--- feelhub-web/src/main/webapp/static/js/modules/bookmarkletinstall.js
+++ feelhub-web/src/main/webapp/static/js/modules/bookmarkletinstall.js
@@ -10,17 +10,24 @@ define(["jquery"],
         $("body").on("click", "#bookmark-help-button-1", function () {
             $("#step-" + step + "." + current_navigator).toggle();
             step = 1;
+            addGoodPlayer();
             $("#step-" + step + "." + current_navigator).toggle();
         });
 
         $("body").on("click", "#bookmark-help-button-2", function () {
             $("#step-" + step + "." + current_navigator).toggle();
+            if (step == 1) {
+                $("#step-" + step + "." + current_navigator + " iframe").remove();
+            }
             step = 2;
             $("#step-" + step + "." + current_navigator).toggle();
         });
 
         $("body").on("click", "#bookmark-help-button-3", function () {
             $("#step-" + step + "." + current_navigator).toggle();
+            if (step == 1) {
+                $("#step-" + step + "." + current_navigator + " iframe").remove();
+            }
             step = 3;
             $("#step-" + step + "." + current_navigator).toggle();
         });
@@ -33,23 +40,35 @@ define(["jquery"],
         function init() {
             if (navigator.userAgent.match(/chrome/i)) {
                 current_navigator = "chrome";
-                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='330' src='//www.youtube.com/embed/uJx0BFmzJto?rel=0&autoplay=1&loop=1&playlist=uJx0BFmzJto' frameborder='0' allowfullscreen></iframe>");
             } else if (navigator.userAgent.match(/firefox/i)) {
                 current_navigator = "firefox";
-                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='295' src='//www.youtube.com/embed/u2M1DABVRo4?rel=0&autoplay=1&loop=1&playlist=u2M1DABVRo4' frameborder='0' allowfullscreen></iframe>");
             } else if (navigator.userAgent.match(/safari/i)) {
                 current_navigator = "safari";
-                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='330' src='//www.youtube.com/embed/CwlzZZHWh14?rel=0&autoplay=1&loop=1&playlist=CwlzZZHWh14' frameborder='0' allowfullscreen></iframe>");
             } else if (navigator.userAgent.match(/opera/i)) {
                 current_navigator = "opera";
             } else {
                 current_navigator = "ie";
             }
-
+            addGoodPlayer();
             $(name).css("left", $(window).width() / 2 - $(name).width() / 2);
             add_responsive_behavior();
             $("#step-" + step + "." + current_navigator).toggle();
         }
+
+        function addGoodPlayer() {
+            if (navigator.userAgent.match(/chrome/i)) {
+                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='330' src='//www.youtube.com/embed/uJx0BFmzJto?rel=0&autoplay=1&loop=1&playlist=uJx0BFmzJto' frameborder='0' allowfullscreen></iframe>");
+            } else if (navigator.userAgent.match(/firefox/i)) {
+                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='295' src='//www.youtube.com/embed/u2M1DABVRo4?rel=0&autoplay=1&loop=1&playlist=u2M1DABVRo4' frameborder='0' allowfullscreen></iframe>");
+            } else if (navigator.userAgent.match(/safari/i)) {
+                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='330' src='//www.youtube.com/embed/CwlzZZHWh14?rel=0&autoplay=1&loop=1&playlist=CwlzZZHWh14' frameborder='0' allowfullscreen></iframe>");
+            } else if (navigator.userAgent.match(/opera/i)) {
+                $("#step-" + step + "." + current_navigator).append("<iframe width='600' height='330' src='//www.youtube.com/embed/RCM-7ct_Oc4?rel=0&autoplay=1&loop=1&playlist=RCM-7ct_Oc4' frameborder='0' allowfullscreen></iframe>");
+            } else {
+
+            }
+        }
+
 
         function add_responsive_behavior() {
 
