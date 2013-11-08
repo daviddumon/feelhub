881e4fb0bc975e3827a916740b6a2460dc65b7d4
david <dumon.david@gmail.com>
2013-11-08 14:57:12 +0100

    script de livraison sitemaps

--- feelhub-sitemap/scripts/sitemap-production-deploy.sh
+++ feelhub-sitemap/scripts/sitemap-production-deploy.sh
@@ -3,4 +3,4 @@ echo 'production-deploy script argument : ' $1
 
 file=feelhub-sitemap-$1-jar-with-dependencies.jar
 
-cp target/$file /opt/sitemaps/sitemaps-builder.jar
+cp feelhub-sitemap/target/$file /opt/sitemaps/sitemaps-builder.jar
