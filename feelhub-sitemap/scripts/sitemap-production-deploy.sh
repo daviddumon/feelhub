7fac565bc61225b179c2847200fa1128ece0b213
charles <couillardcharles@gmail.com>
2013-11-08 22:18:30 +0100

    correction déploiement sitemap

--- feelhub-sitemap/scripts/sitemap-production-deploy.sh
+++ feelhub-sitemap/scripts/sitemap-production-deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo 'production-deploy script argument : ' $1
-echo $(pwd)
 
 file=feelhub-sitemap-$1-jar-with-dependencies.jar
 
-cp feelhub-sitemap/target/$file /opt/sitemaps/sitemaps-builder.jar
+cp target/$file /opt/sitemaps/sitemaps-builder.jar
