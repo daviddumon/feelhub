4cffcddc00b2bea5657cba605d74a673d0b9b6b9
david <dumon.david@gmail.com>
2013-11-19 16:50:22 +0100

    script reprise langues utilisateurs actuels

---
+++ feelhub-web/migrationsjs/035.js
@@ -0,0 +1,7 @@
+db.user.update({_id: BinData(3, "H0eXYInBtXWNq2leXsWRoQ==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "bUqMHT9/gixRIgh4TFTiiQ==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "c0SeoHYip/iPjhmHKgR6mw==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "dUpJTtIyOd5qVIB8DdLrrA==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "hES5BtDnp5f/G/am9sWvsQ==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "nUvDoeAXWKzhoM0Puyp8lg==")}, {$set: {languageCode: "fr"}});
+db.user.update({_id: BinData(3, "6kKcXbQzIdpji+dOo9Nwlg==")}, {$set: {languageCode: "fr"}});
