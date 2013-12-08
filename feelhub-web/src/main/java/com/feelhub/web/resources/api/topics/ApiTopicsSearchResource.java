e172bd08709f057426e7ea0278b0506e5690c617
Charles Couillard <couillardcharles@gmail.com>
2013-12-08 19:36:04 +0100

    préparation de la recherche avancée

--- feelhub-web/src/main/java/com/feelhub/web/resources/api/topics/ApiTopicsSearchResource.java
+++ feelhub-web/src/main/java/com/feelhub/web/resources/api/topics/ApiTopicsSearchResource.java
@@ -3,7 +3,8 @@ package com.feelhub.web.resources.api.topics;
 import com.feelhub.domain.thesaurus.FeelhubLanguage;
 import com.feelhub.domain.topic.Topic;
 import com.feelhub.web.authentification.CurrentUser;
-import com.feelhub.web.dto.*;
+import com.feelhub.web.dto.TopicData;
+import com.feelhub.web.dto.TopicDataFactory;
 import com.feelhub.web.representation.ModelAndView;
 import com.feelhub.web.resources.api.FeelhubApiException;
 import com.feelhub.web.search.TopicSearch;
@@ -12,7 +13,8 @@ import com.feelhub.web.search.criteria.OrderSearchCriteria;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import org.restlet.data.Form;
-import org.restlet.resource.*;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
 
 import java.util.List;
 
@@ -40,26 +42,26 @@ public class ApiTopicsSearchResource extends ServerResource {
                 skip = Integer.parseInt(query.getFirstValue("skip").trim());
             }
         }
-	String order = "order";
-	String name = "from-people";
-	return ModelAndView.createNew("api/topics.json.ftl").with("topicDatas", getTopicDatas(getQueryValue(query, order), getQueryValue(query, name), skip, limit));
+        String order = "order";
+        String name = "from-people";
+        return ModelAndView.createNew("api/topics.json.ftl").with("topicDatas", getTopicDatas(getQueryValue(query, order), getQueryValue(query, name), skip, limit));
     }
 
     private String getQueryValue(Form query, String name) {
-	if (query != null) {
-	    return query.getFirstValue(name, "");
-	}
-	return "";
+        if (query != null) {
+            return query.getFirstValue(name, "");
+        }
+        return "";
     }
 
-    public List<TopicData> getTopicDatas(String order, String fromPeople, final int skip, final int limit) {
+    private List<TopicData> getTopicDatas(String order, String fromPeople, final int skip, final int limit) {
         final List<FeelhubLanguage> languages = Lists.newArrayList();
         languages.add(FeelhubLanguage.reference());
         languages.add(FeelhubLanguage.none());
         if (!CurrentUser.get().getLanguage().isReference() && !CurrentUser.get().getLanguage().isNone()) {
             languages.add(CurrentUser.get().getLanguage());
         }
-	final List<Topic> topics = getTopicsSearch(order, fromPeople, skip, limit, languages).execute();
+        final List<Topic> topics = getTopicsSearch(order, fromPeople, skip, limit, languages).execute();
         final List<TopicData> topicDatas = Lists.newArrayList();
         for (final Topic topic : topics) {
             topicDatas.add(topicDataFactory.topicData(topic, CurrentUser.get().getLanguage()));
@@ -69,9 +71,9 @@ public class ApiTopicsSearchResource extends ServerResource {
 
     private TopicSearch getTopicsSearch(String order, String fromPeople, int skip, int limit, List<FeelhubLanguage> languages) {
         TopicSearch result = getDefaultSearch(skip, limit, languages);
-	OrderSearchCriteria orderCriteria = OrderSearchCriteria.fromString(order);
-	FromPeopleCriteria fromPeopleCriteria = FromPeopleCriteria.fromString(fromPeople);
-	return fromPeopleCriteria.addCriteria(orderCriteria.addCriteria(result));
+        OrderSearchCriteria orderCriteria = OrderSearchCriteria.fromString(order);
+        FromPeopleCriteria fromPeopleCriteria = FromPeopleCriteria.fromString(fromPeople);
+        return fromPeopleCriteria.addCriteria(orderCriteria.addCriteria(result));
     }
 
     private TopicSearch getDefaultSearch(int skip, int limit, List<FeelhubLanguage> languages) {
