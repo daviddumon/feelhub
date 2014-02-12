40bcd902be3a1076c8addb3b14462621e6981d70
david <dumon.david@gmail.com>
2014-02-12 22:25:34 +0100

    conf pom

--- feelhub-sitemap/scripts/sitemap-production-deploy.sh
+++ feelhub-sitemap/scripts/sitemap-production-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo 'production-deploy script argument : ' $1
-
 file=feelhub-sitemap-$1-jar-with-dependencies.jar
-
 cp target/$file /opt/feelhubsitemaps/sitemaps-builder.jar
+chmod +x /opt/feelhubsitemaps/sitemaps-builder.jar
