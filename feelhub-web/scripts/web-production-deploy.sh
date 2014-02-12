e1903f0263f4d0c6fcb1e8223a15da339c8752a3
david <dumon.david@gmail.com>
2014-02-12 22:36:34 +0100

    conf pom

--- feelhub-web/scripts/web-production-deploy.sh
+++ feelhub-web/scripts/web-production-deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 echo 'production-deploy script argument : ' $1
-
-bucket=elasticbeanstalk-us-east-1-952638247896
 file=feelhub-$1.war
-
-# upload war to S3 bucket
-s3cmd put target/$file  s3://$bucket/$file
-
-# create a new application for elastic beanstalk
-/opt/elasticbeanstalk-cli/api/bin/elastic-beanstalk-create-application-version -a feelhub -d $file -l $file -s elasticbeanstalk-us-east-1-952638247896/$file
-
-# use the new version on elastic beanstalk
-/opt/elasticbeanstalk-cli/api/bin/elastic-beanstalk-update-environment -e feelhub -l $file
+cp target/$file /var/lib/tomcat7/feelhub/ROOT.war
+chmod +x /var/lib/tomcat7/feelhub/ROOT.war
--- feelhub-web/scripts/web-test-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'test-deploy script argument : ' $1
-file=feelhub-$1.war
-scp -P 22 target/$file root@10.0.0.7:/opt/tomcat/test.bytedojo.com/feelhub.war
-scp -P 22 scripts/drop.js root@10.0.0.7:
-ssh root@10.0.0.7 'chmod +x /opt/tomcat/test.bytedojo.com/feelhub.war'
